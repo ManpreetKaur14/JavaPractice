9451ae3d829a7f84d6d18c2f1945f6535016a6ca
manpreetkaur15 <manpreetkaur0699@gmail.com>
2021-06-18 18:24:00 +0530

    1234

--- Intrest.java
+++ Intrest.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class intrest 
+public class Intrest 
 {
     public static void main(String[] args) 
     {
---
+++ hello.java
@@ -0,0 +1,10 @@
+class hello
+{ 
+    public static void main(String[] args) 
+    {
+        System.out.println("hello world");
+        System.out.print("hello world ");
+        System.out.print("hello world");
+    
+}
+}
