811ed2123c506df4cfbf40cb8d10f97413bb28ef
manpreetkaur15 <manpreetkaur0699@gmail.com>
2021-06-29 17:36:05 +0530

    sum

---
+++ program2.java
@@ -0,0 +1,40 @@
+// import java.io.*;
+import  java.util.*;
+class program2 
+{
+    public static void main(String[] args) 
+    { 
+        UserMainCode ob = new UserMainCode();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+        int e = sc.nextInt();
+        sc.nextLine();
+        String f = sc.nextLine();
+        int x = ob.countEvensOdds(a,b,c,d,e,f);
+        System.out.println(x);
+    }
+}
+class UserMainCode
+{
+  int countEvensOdds(int input1,int input2,int input3,int input4,int input5,String input6){
+  int count=0;
+  if(input1%2==0)
+   count++;
+  if(input2%2==0)
+   count++;
+  if(input3%2==0)
+   count++;
+  if(input4%2==0)
+   count++;
+  if(input5%2==0)
+   count++;
+  if(input6.equals("even"))
+   return count;
+  else
+   return 5-count;
+ }
+}
